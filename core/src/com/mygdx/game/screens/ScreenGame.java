f0e0fa5c4422bd72f6d7c9a5930908535aa9e6d8
qweenkefi <dumbtao@gmail.com>
2026-04-22 18:37:21 +0300

    first commit

--- core/src/com/mygdx/game/screens/ScreenGame.java
+++ core/src/com/mygdx/game/screens/ScreenGame.java
@@ -2,11 +2,16 @@ package com.mygdx.game.screens;
 
 import com.badlogic.gdx.Screen;
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.objects.MovingBackground;
 
 public class ScreenGame implements Screen {
     private MyGdxGame myGdxGame;
 
+    MovingBackground background;
+
     public ScreenGame (MyGdxGame myGdxGame){
+        this.myGdxGame = myGdxGame;
+
 
     }
     @Override
