219eabd5197b22777d80a6cfd58f983d0192b2c9
qweenkefi <dumbtao@gmail.com>
2026-04-22 18:31:56 +0300

    first commit

--- core/src/com/mygdx/game/GameResources.java
+++ core/src/com/mygdx/game/GameResources.java
@@ -1,4 +1,8 @@
 package com.mygdx.game;
 
 public class GameResources {
+    public static final String BACKGROUND_FONE = "backgrounds/forestBackgroundOne.png";
+    public static final String BACKGROUND_FTWO = "backgrounds/forestBackgroundTwo.png";
+    public static final String BACKGROUND_MENU = "backgrounds/menuBackground.png";
+
 }
